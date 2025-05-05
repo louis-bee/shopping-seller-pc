131aa5cd5a218de1fc3a9f8ae277267dfe620d16
Louis <2720447678@qq.com>
2025-05-06 01:32:57 +0800

    奇怪 dist没提交

--- src/apis/data.js
+++ src/apis/data.js
@@ -1,4 +1,4 @@
-import { request } from "@/utils";
+import { request } from "@/utils/request";
 
 export function getMonthTotalBySellerAPI(params) {
   return request({
--- src/apis/order.js
+++ src/apis/order.js
@@ -1,4 +1,4 @@
-import { request } from "@/utils";
+import { request } from "@/utils/request";
 
 export function getOrderListAPI(params) {
   return request({
